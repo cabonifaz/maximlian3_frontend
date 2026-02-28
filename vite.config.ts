c2a6d1cf63ba7e6dc406d2b3958802dfebe1d1fc
zero <himura.js@hotmail.com>
2026-02-27 22:58:15 -0500

    feat: basic project structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,7 +13,7 @@ export default defineConfig({
 
   resolve: {
     alias: {
-      "@maximilian": "./src",
+      "@maximilian": path.resolve(__dirname, "./src"),
     },
   },
 });
