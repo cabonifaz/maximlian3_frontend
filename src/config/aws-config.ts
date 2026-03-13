db60b03797d4b627fa84d81244fc03247e5db001
zero <himura.js@hotmail.com>
2026-03-12 23:07:00 -0500

    fix(auth): refactor Amplify configuration to use sessionStorage correctly

--- src/config/aws-config.ts
+++ src/config/aws-config.ts
@@ -1,21 +1,20 @@
-import { Amplify } from "aws-amplify";
+import { Amplify, type ResourcesConfig } from "aws-amplify";
 import { sessionStorage } from "aws-amplify/utils";
+import { cognitoUserPoolsTokenProvider } from "aws-amplify/auth/cognito";
 
-Amplify.configure(
-  {
-    Auth: {
-      Cognito: {
-        userPoolId: import.meta.env.VITE_COGNITO_USER_POOL_ID,
-        userPoolClientId: import.meta.env.VITE_COGNITO_CLIENT_ID,
-        loginWith: {
-          username: true,
-        },
-      },
-    },
+/**
+ * See the docs here: https://docs.amplify.aws/gen1/react/build-a-backend/auth/manage-user-session/
+ */
+
+const authConfig: ResourcesConfig["Auth"] = {
+  Cognito: {
+    userPoolId: import.meta.env.VITE_COGNITO_USER_POOL_ID,
+    userPoolClientId: import.meta.env.VITE_COGNITO_CLIENT_ID,
   },
-  {
-    Auth: {
-      tokenStorage: sessionStorage,
-    },
-  },
-);
+};
+
+Amplify.configure({
+  Auth: authConfig,
+});
+
+cognitoUserPoolsTokenProvider.setKeyValueStorage(sessionStorage);
